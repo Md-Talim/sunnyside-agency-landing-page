95bb1cd1ec4a1073ba9bc6a7e8fe25880ec0a801
Mohammad Talim <talimmohammad116@gmail.com>
2023-01-22 20:32:13 +0530

    fixed the link border issue

--- src/components/BorderedLink.js
+++ src/components/BorderedLink.js
@@ -3,7 +3,7 @@ import '../styles/BorderedLink.css';
 const BorderedLink = (props) => (
   <a
     href={props.link}
-    className={`uppercase font-serif font-black after:bg-${props.color}`}
+    className={`uppercase font-serif font-black ${props.color}`}
   >
     {props.text}
   </a>
