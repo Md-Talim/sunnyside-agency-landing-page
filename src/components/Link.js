931f71d489f6b613e5333825e542d1643ff1bc2f
Mohammad Talim <talimmohammad116@gmail.com>
2023-01-24 09:39:23 +0530

    made the link working

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,6 +1,6 @@
 const Link = (props) => (
   <li className='text-lg text-neutral-300 sm:text-neutral-100'>
-    <a href={props.name.toLowerCase()}>{props.name}</a>
+    <a href={`#${props.name.toLowerCase()}`}>{props.name}</a>
   </li>
 );
 
