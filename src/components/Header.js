29cae9e8d281223b888d16207484240316bfaaf6
Mohammad Talim <talimmohammad116@gmail.com>
2023-04-14 05:14:27 +0530

    reduce the margin of arrow

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
 import Nav from './Nav';
 import downArrow from '../images/icon-arrow-down.svg';
 
-
 const Header = () => (
   <header
     className={` bg-mobile-header sm:bg-desktop-header h-screen bg-bottom bg-cover`}
   >
     <Nav />
     <div className="text-center">
-      <h1 className="text-neutral-100 sm:text-6xl text-5xl tracking-[.15em] font-serif font-black uppercase my-20">
+      <h1 className="text-neutral-100 sm:text-6xl text-5xl tracking-[.15em] font-serif font-black uppercase my-16">
         we are creatives
       </h1>
-      <img className="inline-block mt-5" src={downArrow} alt="down arrow" />
+      <img className="inline-block mt-1" src={downArrow} alt="down arrow" />
     </div>
   </header>
 );
